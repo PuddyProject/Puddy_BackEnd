bdbf221d6d3d8c86edb80586a865ec439f5e7beb
waveofmymind <sjun0913@gmail.com>
2023-03-22 18:11:26 +0900

    [FEAT] S3 설정 및 시큐리티 설정
    - SecurityConfig는 필터 미구현으로 현재 모든 경로 접근 가능으로 설정. 
    추후 구현 예정입니다.

--- src/main/java/com/team/puddy/PuddyApplication.java
+++ src/main/java/com/team/puddy/PuddyApplication.java
@@ -2,12 +2,16 @@ package com.team.puddy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class PuddyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PuddyApplication.class, args);
+
 	}
 
 }
---
+++ src/main/java/com/team/puddy/global/config/security/SecurityConfig.java
@@ -0,0 +1,85 @@
+package com.team.puddy.global.config.security;
+
+
+import com.team.puddy.global.config.security.jwt.JwtTokenUtils;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    private final JwtTokenUtils jwtTokenUtils;
+
+    public SecurityConfig(JwtTokenUtils jwtTokenUtils) {
+        this.jwtTokenUtils = jwtTokenUtils;
+    }
+
+    //TODO: JWT 관련 클래스 구현
+
+    // 정적 자원에 대해서는 Security 설정을 적용하지 않음
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .and()
+                .formLogin().disable()
+//                .addFilterBefore(JwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
+//                .exceptionHandling()
+
+                ;
+                //TODO: 로그인, 로그아웃 라우팅
+
+        return http.build();
+    }
+
+
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    //TODO: 배포시 프론트 URL, 백 URL 넣기
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:8080", "http://localhost:3000",
+                "https://localhost:8080", "https://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
