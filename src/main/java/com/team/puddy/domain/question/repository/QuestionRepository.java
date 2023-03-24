1d3e075bc8a63da3f2c55db19b7bffbe9370e71c
waveofmymind <sjun0913@gmail.com>
2023-03-24 13:59:24 +0900

    #4 [FIX] QNA 리스트 관련 로직 수정
    - QuestionRepository: 페이지네이션 조회로 수정
    - QuestionSerivce: 문제 리스트 조회하여 다음페이지 유무도 함께 DTO로 매핑하여 반환하도록 수정

--- src/main/java/com/team/puddy/domain/question/repository/QuestionRepository.java
+++ src/main/java/com/team/puddy/domain/question/repository/QuestionRepository.java
@@ -1,6 +1,7 @@
 package com.team.puddy.domain.question.repository;
 
 import com.team.puddy.domain.question.domain.Question;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,5 +12,5 @@ public interface QuestionRepository extends JpaRepository<Question, Long> {
 
     Optional<Question> findByTitle(String title);
 
-    List<Question> findByOrderByModifiedDateDesc(Pageable pageable);
+    Page<Question> findByOrderByModifiedDateDesc(Pageable pageable);
 }
--- src/main/java/com/team/puddy/domain/question/service/QuestionService.java
+++ src/main/java/com/team/puddy/domain/question/service/QuestionService.java
@@ -2,10 +2,15 @@ package com.team.puddy.domain.question.service;
 
 import com.team.puddy.domain.question.domain.Question;
 import com.team.puddy.domain.question.dto.request.QuestionRequestDto;
+import com.team.puddy.domain.question.dto.response.QuestionListResponseDto;
 import com.team.puddy.domain.question.dto.response.QuestionResponseDto;
 import com.team.puddy.domain.question.repository.QuestionRepository;
+import com.team.puddy.domain.user.domain.User;
+import com.team.puddy.domain.user.repository.UserRepository;
 import com.team.puddy.global.mapper.QuestionMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityNotFoundException;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -23,10 +29,27 @@ public class QuestionService {
 
     private final QuestionRepository questionRepository;
 
-    private final QuestionMapper questionMapper;
+    private final UserRepository userRepository;
 
-    public void addQuestion(QuestionRequestDto requestDto, String imagePath) throws IOException {
-        Question question = questionMapper.toEntity(requestDto, imagePath);
+    private final QuestionMapper questionMapper;
+    @Transactional(readOnly = true)
+    public QuestionListResponseDto getQuestionList(Pageable page) {
+        Page<Question> findQuestionList = questionRepository.findByOrderByModifiedDateDesc(page);
+        boolean hasNextPage = findQuestionList.hasNext();
+        List<QuestionResponseDto> questionResponseDtoList = findQuestionList
+                .stream()
+                .map(questionMapper::toDto)
+                .toList();
+        return QuestionListResponseDto.builder().questionList(questionResponseDtoList)
+                .hasNextPage(hasNextPage)
+                .build();
+
+
+
+    }
+    public void addQuestion(QuestionRequestDto requestDto, String imagePath,Long userId) throws IOException {
+        User findUser = userRepository.findById(userId).orElseThrow(EntityNotFoundException::new);
+        Question question = questionMapper.toEntity(requestDto, imagePath,findUser);
         questionRepository.save(question);
 
     }
@@ -37,12 +60,9 @@ public class QuestionService {
         return questionMapper.toDto(findQuestion);
     }
 
-    public List<QuestionResponseDto> getProblemList(Pageable page) {
-        List<Question> questionList = questionRepository.findByOrderByModifiedDateDesc(page);
-
-        return questionList
-                .stream()
-                .map(questionMapper::toDto)
-                .toList();
+    @Transactional(readOnly = true)
+    public long getQuestionCount() {
+        return questionRepository.count();
     }
+
 }
