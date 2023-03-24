be342497e4172e7a6d00203e4253848f663e2430
waveofmymind <sjun0913@gmail.com>
2023-03-24 14:00:10 +0900

    [FEAT] UserMapper 클래스 생성
    기본 닉네임은 요청에 따라 퍼디1234 설정

---
+++ src/main/java/com/team/puddy/global/mapper/UserMapper.java
@@ -0,0 +1,26 @@
+package com.team.puddy.global.mapper;
+
+import com.team.puddy.domain.type.UserRole;
+import com.team.puddy.domain.user.domain.User;
+import com.team.puddy.domain.user.dto.request.LoginUserRequest;
+import com.team.puddy.domain.user.dto.request.RegisterUserRequest;
+import org.mapstruct.Mapper;
+import org.mapstruct.NullValueCheckStrategy;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring",
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
+public interface UserMapper {
+
+    default User toEntity(RegisterUserRequest request){
+        return User.builder()
+                .account(request.account())
+                .password(request.password())
+                .email(request.email())
+                .username(request.username())
+                .role(UserRole.USER.getRole())
+                .nickname("퍼디1234")
+                .build();
+    }
+}
