e332682fd21532679207dc41d6594e28a0cc18a6
waveofmymind <sjun0913@gmail.com>
2023-03-29 21:11:43 +0900

    [FIX] 질문글 상세 조회시 답변글도 응답하도록 수정

--- src/main/java/com/team/puddy/domain/question/dto/response/QuestionResponseDto.java
+++ src/main/java/com/team/puddy/domain/question/dto/response/QuestionResponseDto.java
@@ -1,18 +1,24 @@
 package com.team.puddy.domain.question.dto.response;
 
+import com.team.puddy.domain.answer.dto.ResponseAnswerDto;
 import com.team.puddy.global.mapper.validator.Category;
 import lombok.*;
 
 import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+import java.util.List;
 
 
-public record QuestionResponseDto (String title,
+public record QuestionResponseDto (Long questionId,
+                                   String title,
                                    String content,
                                    String nickname,
+                                   LocalDateTime createdDate,
                                    @Category String category,
                                    long viewCount,
                                    boolean isSolved,
-                                   int postCategory) {
+                                   int postCategory,
+                                   List<ResponseAnswerDto> answerList) {
     @Builder
     public QuestionResponseDto {}
 }
