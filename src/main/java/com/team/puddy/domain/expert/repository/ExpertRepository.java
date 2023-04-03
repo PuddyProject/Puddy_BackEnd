41cbf029e70624f22429da198711cf1b2988b87d
waveofmymind <sjun0913@gmail.com>
2023-04-04 00:01:11 +0900

    #14 [FEAT] 메인 페이지 응답 데이터 추가

--- src/main/java/com/team/puddy/domain/expert/repository/ExpertRepository.java
+++ src/main/java/com/team/puddy/domain/expert/repository/ExpertRepository.java
@@ -1,8 +1,12 @@
 package com.team.puddy.domain.expert.repository;
 
 import com.team.puddy.domain.expert.domain.Expert;
+import com.team.puddy.domain.question.domain.Question;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ExpertRepository extends JpaRepository<Expert, Long> {
@@ -10,4 +14,7 @@ public interface ExpertRepository extends JpaRepository<Expert, Long> {
     boolean existsByUserId(Long id);
 
     Optional<Expert> findByUserId(Long userId);
+
+    List<Expert> findTop5ByOrderByCreatedDateDesc();
+
 }
--- src/main/java/com/team/puddy/domain/question/dto/response/MainPageResponseDto.java
+++ src/main/java/com/team/puddy/domain/question/dto/response/MainPageResponseDto.java
@@ -1,12 +1,15 @@
 package com.team.puddy.domain.question.dto.response;
 
+import com.team.puddy.domain.expert.dto.ResponseExpertDto;
 import com.team.puddy.domain.question.dto.response.QuestionResponseDto;
 import lombok.Builder;
 
 import java.util.List;
 
 public record MainPageResponseDto(List<QuestionResponeDtoExcludeAnswer> popularQuestions,
-                                      List<QuestionResponeDtoExcludeAnswer> recentQuestions) {
+                                      List<QuestionResponeDtoExcludeAnswer> recentQuestions,
+                                  List<ResponseExpertDto> recentExperts,
+                                  boolean hasPet) {
     @Builder
     public MainPageResponseDto {
     }
