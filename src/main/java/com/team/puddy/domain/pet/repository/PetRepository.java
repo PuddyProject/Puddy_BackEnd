23db01232e6372c00a9b5e6467d34b5dac0628a1
waveofmymind <sjun0913@gmail.com>
2023-04-05 19:28:46 +0900

    #19 [FEAT] 펫 수정 메소드 추가

--- src/main/java/com/team/puddy/domain/pet/repository/PetRepository.java
+++ src/main/java/com/team/puddy/domain/pet/repository/PetRepository.java
@@ -3,5 +3,8 @@ package com.team.puddy.domain.pet.repository;
 import com.team.puddy.domain.pet.domain.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PetRepository extends JpaRepository<Pet, Long> {
+    Optional<Pet> findPetByUserId(Long userId);
 }
--- src/main/java/com/team/puddy/domain/pet/service/PetService.java
+++ src/main/java/com/team/puddy/domain/pet/service/PetService.java
@@ -2,6 +2,7 @@ package com.team.puddy.domain.pet.service;
 
 import com.team.puddy.domain.pet.domain.Pet;
 import com.team.puddy.domain.pet.dto.request.RequestPetDto;
+import com.team.puddy.domain.pet.dto.request.UpdatePetDto;
 import com.team.puddy.domain.pet.dto.response.ResponsePetDto;
 import com.team.puddy.domain.pet.repository.PetQueryRepository;
 import com.team.puddy.domain.pet.repository.PetRepository;
@@ -11,12 +12,14 @@ import com.team.puddy.global.error.ErrorCode;
 import com.team.puddy.global.error.exception.NotFoundException;
 import com.team.puddy.global.mapper.PetMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class PetService {
 
     private final UserRepository userRepository;
@@ -31,10 +34,14 @@ public class PetService {
         findUser.setPet(petRepository.save(petMapper.toEntity(findUser,request,imagePath)));
     }
 
-
+    @Transactional(readOnly = true)
     public ResponsePetDto getPetByUserId(Long userId) {
         Pet findPet = petQueryRepository.findPetByUserId(userId).orElseThrow(() -> new NotFoundException(ErrorCode.PET_NOT_FOUND));
 
         return petMapper.toDto(findPet);
+    }
+    public void updatePet(UpdatePetDto updateDto, Long userId,String imagePath) {
+        Pet findPet = petRepository.findPetByUserId(userId).orElseThrow(() -> new NotFoundException(ErrorCode.PET_NOT_FOUND));
+        findPet.updatePet(updateDto,imagePath);
     }
 }
