d66165079159a0b828c1f3aa968c48f4b42b19f4
waveofmymind <sjun0913@gmail.com>
2023-04-09 10:06:37 +0900

    [FEAT] 답변글 수정, 삭제 로직 구현

--- src/main/java/com/team/puddy/domain/answer/repository/AnswerQueryRepository.java
+++ src/main/java/com/team/puddy/domain/answer/repository/AnswerQueryRepository.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import static com.team.puddy.domain.answer.domain.QAnswer.answer;
 import static com.team.puddy.domain.user.domain.QUser.user;
@@ -28,9 +29,25 @@ public class AnswerQueryRepository {
     public List<Answer> findAnswerListByUserId(Long userId) {
 
         return queryFactory.selectFrom(answer)
-                .innerJoin(answer.user, user)
+                .leftJoin(answer.user, user)
                 .fetchJoin()
                 .where(user.id.eq(userId))
                 .fetch();
+    }
+
+    public Optional<Answer> findAnswerForUpdate(Long answerId,Long questionId,Long userId) {
+        return Optional.ofNullable(queryFactory.selectFrom(answer)
+                .where(answer.id.eq(answerId)
+                        .and(user.id.eq(userId))
+                        .and(answer.question.id.eq(questionId))) // 두 개의 조건을 연결
+                .fetchOne());
+    }
+
+    public boolean existsAnswer(Long answerId, Long questionId, Long userId) {
+        return queryFactory.selectFrom(answer)
+                .where(answer.id.eq(answerId)
+                        .and(answer.user.id.eq(userId))
+                        .and(answer.question.id.eq(questionId)))
+                .fetchOne() != null;
     }
 }
