ac369c943e39f39e4c384570856e9b0c206eb7ec
waveofmymind <sjun0913@gmail.com>
2023-04-12 16:50:40 +0900

    [FEAT] 유저 아이디 찾기 경로 추가

--- src/main/java/com/team/puddy/domain/user/repository/UserRepository.java
+++ src/main/java/com/team/puddy/domain/user/repository/UserRepository.java
@@ -1,13 +1,16 @@
 package com.team.puddy.domain.user.repository;
 
+import com.team.puddy.domain.type.JwtProvider;
 import com.team.puddy.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
 
     Optional<User> findByAccount(String account);
+    Optional<User> findByAccountAndProvider(String account, JwtProvider jwtProvider);
 
-
+    Optional<User> findByUsernameAndEmail(String username, String email);
 }
